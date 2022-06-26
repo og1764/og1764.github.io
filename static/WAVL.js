c534934e34fe270d2b7e48549d4232eb1d0191ef
og1764 <itstheguaz@gmail.com>
2022-06-26 11:18:56 +0800

    typo more plz

--- static/WAVL.js
+++ static/WAVL.js
@@ -786,7 +786,7 @@ function pdf_init(venues, wavl, wavjl, date) {
         console.log(date);
         for (var j = 0; j < 6; j++) {
           var date_time = $("#DatePicker2").datepicker("getDate");
-          date_time.setDate(date_time.getDate() - j;
+          date_time.setDate(date_time.getDate() - j);
           console.log(date_time);
           for (var i = 0; i < leagues.length; i++) {
             var indiv = get_single_fixture(venues, leagues[i], date_time);
